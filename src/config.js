fae0891e311cb4d8c3c729afa628be401feca8fe
Ezekiel <ezekiel@gmail.com>
2025-05-02 20:13:11 +0800

    Updated XYZ feature / bugfix / etc

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 
 const config = {
-    API_URL: process.env.REACT_APP_API_URL, // Use the environment variable
+    REACT_APP_API_URL: process.env.REACT_APP_API_URL, // Use the environment variable
     GOOGLE_CLIENT_ID: '824956744352-a4sj5egukjh1csk8galsalp6v4i73gbq.apps.googleusercontent.com', // Replace with your actual Google Client ID
 };
 
