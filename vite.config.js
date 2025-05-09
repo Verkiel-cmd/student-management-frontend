4fb671cb42b5f0318edd5beadf769adbf01a69d3
Ezekiel <ezekiel@gmail.com>
2025-05-09 16:37:09 +0800

    Updated XYZ feature / bugfix / etc

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    headers: {
+        'Content-Security-Policy': `
+            default-src 'self';
+            script-src 'self' https://accounts.google.com https://apis.google.com https://cdn.jsdelivr.net;
+            style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net;
+            connect-src 'self' https://student-management-backend-a2q4.onrender.com https://play.google.com https://accounts.google.com;
+            img-src 'self' https://*.googleusercontent.com;
+            frame-src 'self' https://accounts.google.com;
+        `,
+    },
+},
+});
