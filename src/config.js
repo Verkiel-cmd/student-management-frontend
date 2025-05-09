0a32f6d3ec2fa4c77ff9db7dfbaa4b14a3ea5325
Ezekiel <ezekiel@gmail.com>
2025-05-09 16:12:08 +0800

    Updated XYZ feature / bugfix / etc

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 const config = {
     API_URL: import.meta.env.VITE_API_URL, // Use Vite's environment variable
-    GOOGLE_CLIENT_ID: import.meta.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
 };
 
 export default config;
