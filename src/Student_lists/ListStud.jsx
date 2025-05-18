80c64b8c396bddb75c04a2409e39988feddf9dd5
Ezekiel <ezekiel@gmail.com>
2025-05-18 21:49:51 +0800

    Updated XYZ feature / bugfix / etc

--- src/Student_lists/ListStud.jsx
+++ src/Student_lists/ListStud.jsx
@@ -293,21 +293,19 @@ function ListStud() {
               style={{ zIndex: 3100 }}
             >
               <li>
-                <a
-                  href="#"
-                  className="dropdown-item px-3 py-2 text-dark hover:bg-gray-100 focus:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400"
-                >
-                  Settings
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="dropdown-item px-3 py-2 text-dark hover:bg-gray-100 focus:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400"
-                  onClick={handleLogout}
-                >
-                  Logout
-                </a>
+               <a
+  href="#"
+  className="dropdown-item px-3 py-2 text-dark hover:bg-gray-100 focus:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300"
+>
+  Settings
+</a>
+<a
+  href="/"
+  className="dropdown-item px-3 py-2 text-dark hover:bg-gray-100 focus:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300"
+  onClick={handleLogout}
+>
+  Logout
+</a>
               </li>
             </ul>
           )}
