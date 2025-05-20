07592e38d0a91fcf0afa638fcafd5d4c986f0e94
Ezekiel <ezekiel@gmail.com>
2025-05-20 17:51:17 +0800

    Updated XYZ feature / bugfix / etc

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,6 @@ export const content = [
   "./src/**/*.{js,jsx,ts,tsx}", // Add this line
 ];
 export const theme = {
-  extend: {},
+  extend: { fontFamily: { sans: ['"Segoe UI"', 'sans-serif']}},
 };
 export const plugins = [];
